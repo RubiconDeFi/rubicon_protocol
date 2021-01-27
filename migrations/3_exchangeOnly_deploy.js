3204f2a4409ebbe1f76336fad99b6f7cfea6df4f
unknown <benjamin.hughes@utexas.edu>
2021-01-26 18:50:17 -0600

    commented out WETH tests due to no longer being needed for testing

--- migrations/3_exchangeOnly_deploy.js
+++ migrations/3_exchangeOnly_deploy.js
@@ -5,5 +5,5 @@ module.exports = function(deployer, network, accounts) {
 
     //Toggle below line for single contract deployment
     //truffle migrate -f 3 --network kovan
-    deployer.deploy(RubiconMarket, 22446539696, false, process.env.MAINNET_MULTISIG_RUBICON);
+    // deployer.deploy(RubiconMarket, 22446539696, false, process.env.MAINNET_MULTISIG_RUBICON);
 };
