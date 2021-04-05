c1f63df3706117fa2bca0d3f9dfee78f3111e057
bghughes <benjamin@rubicon.finance>
2021-04-05 17:42:50 -0500

    clean ds store

--- migrations/3_exchangeOnly_deploy.js
+++ migrations/3_exchangeOnly_deploy.js
@@ -6,4 +6,11 @@ module.exports = function(deployer, network, accounts) {
     //Toggle below line for single contract deployment
     //truffle migrate -f 3 --network kovan
     // deployer.deploy(RubiconMarket, 22446539696, false, process.env.MAINNET_MULTISIG_RUBICON);
+
+    // TODO: deploy exchange and all of pools in Proxies here and make test changes...
+    // deployer.deploy(RubiconMarket).then(function() {
+    //     return deployer.deploy(BathHouse, RubiconMarket.address).then(function() {
+    //       return deployer.deploy(Strategy, "Stoikov Market Making", BathHouse.address, RubiconMarket.address);
+    //     }); 
+    // });
 };
