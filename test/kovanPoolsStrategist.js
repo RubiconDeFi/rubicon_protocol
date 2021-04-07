dbf854178376a81bcc3e1fbcfc74d385783f7db3
bghughes <benjamin@rubicon.finance>
2021-04-07 15:21:31 -0500

    add RubiconMarket to pools strategist file

--- test/kovanPoolsStrategist.js
+++ test/kovanPoolsStrategist.js
@@ -1,9 +1,19 @@
 const Web3 = require('web3');
+// var Contract = require('web3-eth-contract');
+var fs = require('fs');
 require("dotenv");
 
-let web3 = new Web3("ws://localhost:8545");
+// Initialize Web3
+let web3 = new Web3("https://kovan.infura.io/v3/2989f59dba68436a9c9221bc6d1603d4");
+// console.log("Web3 Version: ", web3.version);
 
-console.log("Web3 Version: ", web3.version);
+// Load the RubiconMarket contract
+var { abi } = require("../build/contracts/RubiconMarket.json");
+var rubiconMarketKovanAddr = "0x8A4A4d9A874dc038c01638e003fF7307498103f6";
+var RubiconMarketContractKovan = new web3.eth.Contract(abi, rubiconMarketKovanAddr);
+
+// Load in Pools contract addresses on Kovan
+
 
 // Pseudocode - As a loop:
 // 1. Grab the current price for a Kovan pair
