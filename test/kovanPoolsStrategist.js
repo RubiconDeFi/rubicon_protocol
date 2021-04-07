7fbbdb9e226b6694ab7c6ebc2ef8384ad44cc591
bghughes <benjamin@rubicon.finance>
2021-04-07 16:50:42 -0500

    added logic for initBathPairs()

--- test/kovanPoolsStrategist.js
+++ test/kovanPoolsStrategist.js
@@ -1,10 +1,10 @@
 const Web3 = require('web3');
 // var Contract = require('web3-eth-contract');
 var fs = require('fs');
-require("dotenv");
+require("dotenv").config();
 
 // Initialize Web3
-let web3 = new Web3("https://kovan.infura.io/v3/2989f59dba68436a9c9221bc6d1603d4");
+let web3 = new Web3("https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY);
 // console.log("Web3 Version: ", web3.version);
 
 // Load the RubiconMarket contract
@@ -20,6 +20,21 @@ var bathHouseContractKovan = new web3.eth.Contract(abi, bathHouseKovanAddr);
 var { abi } = require("../build/contracts/Strategy.json");
 var strategyKovanAddr = "0xFC827B5157cAE59Ed96A2E0537B7C2EF9939306B";
 var strategyContractKovan = new web3.eth.Contract(abi, strategyKovanAddr);
+
+// Initialize a bathPair for WAYNE / DAI
+var txData = bathHouseContractKovan.methods.initBathPair("0xC61812684385910CF8E93Fa0B04c572E6051F679", "WAYNE", "0x7f21271358765A4b04dB20Ba0BBFE309EC91259a", "DAI").encodeABI();
+
+var tx = {
+    gas: 12500000,
+    data: txData.toString(),
+    from: process.env.KOVAN_DEPLOYER_ADDRESS.toString(),
+    to: bathHouseKovanAddr
+}
+web3.eth.accounts.signTransaction(tx, process.env.PRIVATE_KEY_KOVAN).then((signedTx) => {
+    // console.log(signedTx);
+    web3.eth.sendSignedTransaction(signedTx.rawTransaction).on('receipt', console.log);
+});
+
 
 // Pseudocode - As a loop:
 // 1. Grab the current price for a Kovan pair
