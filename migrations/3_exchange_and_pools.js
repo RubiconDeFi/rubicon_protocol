e23ba5a2d9accef309040a52bea29eea6dc262dd
bghughes <benjamin@rubicon.finance>
2021-04-07 15:38:12 -0500

    add Kovan Pools migrations

--- migrations/3_exchange_and_pools.js
+++ migrations/3_exchange_and_pools.js
@@ -7,13 +7,27 @@ const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 
 // This file will deploy Rubicon Market and Pools while wrapping everything in upgradeable proxies
 module.exports = async function(deployer, network, accounts) {
-
-    //Use below line for single contract deployment
+    //Use below line for only migrating this file
     //truffle migrate -f 3 --network kovan
+
+    // Deploy the Exchange, Pools, and main Strategy
     // TODO: wrap this in upgradeable proxies
-    deployer.deploy(RubiconMarket).then(function() {
-          return deployer.deploy(BathHouse, RubiconMarket.address).then(function() {
+    if (network == 'development' || network == 'pools'){
+    await deployer.deploy(RubiconMarket).then(function() {
+          return deployer.deploy(BathHouse).then(function() {
             return deployer.deploy(Strategy, "Stoikov Market Making", BathHouse.address, RubiconMarket.address);
           }); 
       });
+      }
+
+    // Deploy Pools and Strategy pointed at existing Kovan Exchange
+    if (network == 'kovan'){
+      await  deployer.deploy(BathHouse).then(function() {
+        return deployer.deploy(Strategy, "Stoikov Market Making", BathHouse.address, process.env.RUBICONMARKET_V0_KOVAN);
+         }); 
+      bathHouseInstance = await BathHouse.deployed();
+      await bathHouseInstance.initialize(process.env.RUBICONMARKET_V0_KOVAN);
+      console.log('Kovan BathHouse Address: ', BathHouse.address);
+      console.log('Kovan Strategy Address: ', Strategy.address);
+    }
 };
