146ff2b0adaf8f4abfadf5ecdc4175c9c0913596
bghughes <benjamin@rubicon.finance>
2021-04-20 16:24:38 -0500

    implement dynamic inventory sizing

--- contracts/rubiconPoolsv0/BathPair.sol
+++ contracts/rubiconPoolsv0/BathPair.sol
@@ -518,14 +518,13 @@ contract BathPair {
         );
 
         // Enforce dynamic ordersizing and inventory management
-        emit LogNote("maxOrderSize Require", getMaxOrderSize(underlyingAsset, bathAssetAddress)); //49500000000000000
-        emit LogNote("maxOrderSize Require", getMaxOrderSize(underlyingQuote, bathQuoteAddress)); //49500000000000000
-        emit LogNote("actual size", askNumerator); //100000000000000000
-        emit LogNoteI("midpointPrice", getMidpointPrice());
-        // require(askNumerator <= getMaxOrderSize(underlyingAsset, bathAssetAddress), "the ask is too large in size");
-        // require(bidNumerator <= getMaxOrderSize(underlyingQuote, bathQuoteAddress), "the bid is too large in size");
-        // getMaxOrderSize(underlyingAsset, bathAssetAddress);
-        // getMaxOrderSize(underlyingQuote, bathQuoteAddress);
+        // emit LogNote("maxOrderSize Require", getMaxOrderSize(underlyingAsset, bathAssetAddress)); //49500000000000000
+        // emit LogNote("maxOrderSize Require", getMaxOrderSize(underlyingQuote, bathQuoteAddress)); //49500000000000000
+        // emit LogNote("actual size", askNumerator); //100000000000000000
+        // emit LogNoteI("midpointPrice", getMidpointPrice());
+
+        require(askNumerator <= getMaxOrderSize(underlyingAsset, bathAssetAddress), "the ask is too large in size");
+        require(bidNumerator <= getMaxOrderSize(underlyingQuote, bathQuoteAddress), "the bid is too large in size");
 
         // 1. Enforce that a spread exists and that the ask price > best bid price && bid price < best ask price
         enforceSpread(
