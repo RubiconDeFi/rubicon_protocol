b5aa707733d01e1731882fc7c42a700ed9d19805
bghughes <benjamin@rubicon.finance>
2021-04-27 15:58:58 -0500

    remove unnecesary Strategy imports

--- contracts/rubiconPoolsv0/Strategy.sol
+++ contracts/rubiconPoolsv0/Strategy.sol
@@ -6,10 +6,8 @@ pragma solidity ^0.5.16;
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import "./BathToken.sol";
 import "../RubiconMarket.sol";
-import "../peripheral_contracts/SafeMath.sol";
 import "./BathHouse.sol";
 import "./BathPair.sol";
-import "../peripheral_contracts/ABDKMath64x64.sol";
 
 contract Strategy {
     string public name;
