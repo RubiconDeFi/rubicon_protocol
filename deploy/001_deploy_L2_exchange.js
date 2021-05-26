c7c2934c811cc99185783999bfc490f5ad806abf
bghughes <benjamin@rubicon.finance>
2021-05-26 13:05:22 -0500

    RubiconMarket OP Kovan deployment

--- deploy/001_deploy_L2_exchange.js
+++ deploy/001_deploy_L2_exchange.js
@@ -1,23 +1,27 @@
-// import {HardhatRuntimeEnvironment} from 'hardhat/types';
-// import {DeployFunction} from 'hardhat-deploy/types';
-
-// const { ethers } = require("hardhat")
-
-// Just a standard hardhat-deploy deployment definition file!
+// Rubicon Market L2 Deploy and init
 const func = async (hre) => {
-    const { deployments, getNamedAccounts } = hre
-    const { deploy } = deployments
-    const { deployer } = await getNamedAccounts()
-    console.log("deployer", deployer)
-    // const sender = new ethers.Wallet(process.env.PRIVATE_KEY_KOVAN, new ethers.providers.JsonRpcProvider('https://kovan.optimism.io'))
-    
-    await deploy('WETH9', { 
-      from: deployer,
-      gasPrice: hre.ethers.BigNumber.from('0'),
-      gasLimit: 8999999,
-      log: true
-    })
-  }
+  const { deployments, getNamedAccounts } = hre
+  const { deploy } = deployments
+  const { deployer } = await getNamedAccounts()
 
-  func.tags = ['WETH9'] // Replace `ERC20` with your contract's file name
-  module.exports = func
+  const deployResult = await deploy('RubiconMarket', {
+    from: deployer,
+    log: true
+  });
+  if (deployResult.newlyDeployed) {
+    log(
+      `contract RubiconMarket deployed at ${deployResult.contract.address} using ${deployResult.receipt.gasUsed} gas`
+    );
+  }
+  // const rubiconMarketInstance = await deployments.get('RubiconMarket');
+  // console.log(rubiconMarketInstance);
+
+  // Initialize Rubicon Market
+  const rmi = await hre.ethers.getContractFactory("RubiconMarket");
+  const RMI = await rmi.attach("0x5D0e5aa14399FDb06aC9fE6eAcE32ff259b70Ea0");
+  await RMI.initialize(false, "0xC96495C314879586761d991a2B68ebeab12C03FE", {gasLimit: 8999999}).then((r ) => console.log(r));
+
+}
+
+func.tags = ['RubiconMarket']
+module.exports = func
