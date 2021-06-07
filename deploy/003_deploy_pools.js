ef8fdce842f0c720980b4a4105f7dd9107bd697e
unknown <benjamin.hughes@utexas.edu>
2021-06-07 14:13:50 -0500

    successful pools deploy OP kovan

--- deploy/003_deploy_pools.js
+++ deploy/003_deploy_pools.js
@@ -6,6 +6,7 @@ const func = async (hre) => {
   const { deploy } = deployments
   const { deployer } = await getNamedAccounts()
 
+// -----------------------------------------------
   // Deploy and init Bath House
   const deployResultBH = await deploy('BathHouse', {
     from: deployer,
@@ -16,8 +17,7 @@ const func = async (hre) => {
       `contract BathHouse deployed at ${deployResultBH.address}`
     );
 
-    // // Init BathHouse
-      // If already deployed 
+    // Init BathHouse
         const bh = await hre.ethers.getContractFactory("BathHouse");
         const BHI = await bh.attach(deployResultBH.address);
         await BHI.estimateGas.initialize(process.env.OP_KOVAN_MARKET, 80, 259200, 10).then(async function(g) {
@@ -37,35 +37,29 @@ const func = async (hre) => {
     //   await BHI.initialize(process.env.OP_KOVAN_MARKET, 80, 259200, 10, {gasLimit: g._hex}).then((r) => console.log("BH Init \n" + r));
     // });
 
-  setTimeout(() => {}, 8000);
-  // Deploy BathToken for WAYNE
-  const bathTokenFactory = await hre.ethers.getContractFactory("BathToken");
-  const bathTokenWAYNE = await bathTokenFactory.deploy().then((r) => console.log(r));
-  // await bathTokenWAYNE.initialize("bathWAYNE", process.env.OP_KOVAN_WAYNE, process.env.OP_KOVAN_MARKET, process.env.OP_KOVAN_BATHHOUSE, {gasLimit: 8999999}).then((r ) => console.log("BathWAYNE deployed and init \n" + r));
-    
-  setTimeout(() => {}, 8000);
-     // Deploy BathToken for USDC
-  const bathTokenUSDC = await bathTokenFactory.deploy().then((r) => console.log(r));
-  // await bathTokenUSDC.initialize("bathUSDC", process.env.OP_KOVAN_USDC, process.env.OP_KOVAN_MARKET, process.env.OP_KOVAN_BATHHOUSE, {gasLimit: 8999999}).then((r ) => console.log("BathUSDC deployed and init \n" +r));
- 
-
-  // const deployResultBtW = await deploy('BathToken', {
-  //   from: deployer,
-  //   log: true,
-  //   gasLimit: 9000000
-  // });
-  // if (deployResultBP.newlyDeployed) {
-  //   console.log(
-  //     `contract BathPair deployed at ${deployResultBP.address}`
-  //   );
-
-  //       // Load BathPair if not deployed
-  //       const bp = await hre.ethers.getContractFactory("BathPair");
-  //       const BPI = await bp.attach(deployResultBP.address);
-  //       await BPI.initialize(bathAssetAddr, bathQuoteAddr,
-  //           BHI.address, {gasLimit: 8999999}).then((r ) => console.log(r));
-  // }
-
+// -----------------------------------------------
+  // Deploy BathTokens
+//   // Deploy BathToken for WAYNE
+//   const bathTokenFactory = await hre.ethers.getContractFactory("BathToken");
+//   const bathTokenWAYNE = await bathTokenFactory.deploy().then(async function(r) {
+//     console.log("bathWAYNE deployed at " + r.address);
+//     const btWAddr = r.address;
+//     const btWInst = await bathTokenFactory.attach(r.address);
+//     await btWInst.estimateGas.initialize("bathWAYNE", process.env.OP_KOVAN_WAYNE, process.env.OP_KOVAN_MARKET, process.env.OP_KOVAN_BATHHOUSE, {gasLimit: 8999999}).then(async function (g) {
+//       await btWInst.initialize("bathWAYNE", process.env.OP_KOVAN_WAYNE, process.env.OP_KOVAN_MARKET, process.env.OP_KOVAN_BATHHOUSE, {gasLimit: g._hex}).then((r) => console.log("init of Bath token", r));
+//   });
+// });
+      
+// // Deploy BathToken for USDC
+//      const bathTokenUSDC = await bathTokenFactory.deploy().then(async function(r) {
+//       console.log("bathUSDC deployed at " + r.address);
+//       const btUAddr = r.address;
+//       const btUInst = await bathTokenFactory.attach(r.address);
+//       await btUInst.estimateGas.initialize("bathUSDC", process.env.OP_KOVAN_USDC, process.env.OP_KOVAN_MARKET, process.env.OP_KOVAN_BATHHOUSE, {gasLimit: 8999999}).then(async function (g) {
+//         await btUInst.initialize("bathWAYNE", process.env.OP_KOVAN_WAYNE, process.env.OP_KOVAN_MARKET, process.env.OP_KOVAN_BATHHOUSE, {gasLimit: g._hex}).then((r) => console.log("init of Bath token", r));
+//     });
+//   });
+// -----------------------------------------------
 
   // Deploy BathPair for WAYNE / DAI
   const deployResultBP = await deploy('BathPair', {
@@ -78,11 +72,15 @@ const func = async (hre) => {
       `contract BathPair deployed at ${deployResultBP.address}`
     );
 
-        // Load BathPair if not deployed
-        const bp = await hre.ethers.getContractFactory("BathPair");
-        const BPI = await bp.attach(deployResultBP.address);
-        await BPI.initialize(bathTokenWAYNE.address, bathTokenUSDC.address,
-            BHI.address, {gasLimit: 8999999}).then((r ) => console.log(r));
+    // Load BathPair if not deployed
+    const bp = await hre.ethers.getContractFactory("BathPair");
+    const BPI = await bp.attach(deployResultBP.address);
+    await BPI.initialize(process.env.OP_KOVAN_BATHWAYNE, process.env.OP_KOVAN_BATHUSDC,
+        process.env.OP_KOVAN_BATHHOUSE, {gasLimit: 5000000}).then(async function(g) {
+          console.log(g);
+          // await BPI.initialize(process.env.OP_KOVAN_BATHWAYNE, process.env.OP_KOVAN_BATHUSDC,
+          //   process.env.OP_KOVAN_BATHHOUSE, {gasLimit: g._hex}).then((r) => console.log("init of BP", r));
+        });
   }
 }
 
