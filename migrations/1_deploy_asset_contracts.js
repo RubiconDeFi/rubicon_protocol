1092647a73749aadf105b94b6a2eed24aa3994f2
bghughes <benjamin.hughes@utexas.edu>
2021-09-25 12:27:34 -0500

    clean v1.1

--- migrations/1_deploy_asset_contracts.js
+++ migrations/1_deploy_asset_contracts.js
@@ -1,15 +1,5 @@
 var WETH = artifacts.require("./contracts/WETH9.sol");
 var DAI = artifacts.require("./contracts/peripheral_contracts/USDCWithFaucet.sol");
-var WAYNE = artifacts.require("./contracts/peripheral_contracts/EquityToken.sol");
-var STARK = artifacts.require("./contracts/peripheral_contracts/EquityToken.sol");
-
-var GME = artifacts.require("./contracts/peripheral_contracts/EquityToken.sol");
-var OPT = artifacts.require("./contracts/peripheral_contracts/EquityToken.sol");
-var SPXE = artifacts.require("./contracts/peripheral_contracts/EquityToken.sol");
-var WBTC = artifacts.require("./contracts/peripheral_contracts/EquityToken.sol");
-var COIN = artifacts.require("./contracts/peripheral_contracts/EquityToken.sol");
-
-const BigNumber = require('bignumber.js');
 
 module.exports = function(deployer, network, accounts) {
   var admin = accounts[0];
