b233360b604133bb962a5b9233c8a1e4e7ed26ef
bghughes <benjamin.hughes@utexas.edu>
2021-10-15 14:07:14 -0500

    ERC20

--- contracts/peripheral_contracts/ERC20.sol
+++ contracts/peripheral_contracts/ERC20.sol
@@ -2,9 +2,9 @@
 
 pragma solidity >=0.6.0 <0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/../../utils/Context.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
 import "./IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/../../math/SafeMath.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 /**
  * @dev Implementation of the {IERC20} interface.
